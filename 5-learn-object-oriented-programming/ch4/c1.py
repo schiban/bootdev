5d73d042003afb5086ebd5fa5bca6053e46aa248
schiban <j_bandeira_@hotmail.com>
2025-01-03 16:21:51 +0000

    Add inheritance challenge 1

---
+++ 5-learn-object-oriented-programming/ch4/c1.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def get_area(self):
+        return self.length * self.width
+
+    def get_perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
